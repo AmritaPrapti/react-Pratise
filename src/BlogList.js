27628d60c77d303132263d368db735e8d6f5d342
unknown <amritapprapti@gmail.com>
2022-03-13 16:32:43 +0600

    all functionalities

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import DeleteIcon from "./icon-delete";
+
 const BlogList = ({blogs}) => {
-	// const blogs = props.blogs;
+	const [blogData, setBlogData] = useState(blogs)
 	const navigate = useNavigate();
 	
 	
-	const handleClick = (id) => {
+	const handleDelete = (id) => {
 	
 		fetch('http://localhost:8000/blogs/' + id , { 
 			method: 'DELETE'
 		}).then((res) => {
-			console.log(res)
+			// console.log(res)
 			navigate('/');
+			fetch('http://localhost:8000/blogs')
+			.then(response => response.json())
+			.then(response => setBlogData(response))	
 		})
 	}
 	return (
 		<div className="blogs">
-			{blogs.map(({title,author,id}) => (
+			{blogData.map(({title,author,id}) => (
 				<div className="blogs__content" key={id}>
 					<div className="blog__col">
 						<Link to={`/blogs/${id}`} className="text--links"><h4 className="title title--blog">{title}</h4></Link>
@@ -24,10 +29,12 @@ const BlogList = ({blogs}) => {
 						<p className="text--blog">- Written by {author}</p>
 					</div>
 					<div className="blog__col">
-						<button className="btn btn--delete" onClick={() => handleClick(id)} ><DeleteIcon /></button>
+						<button className="btn btn--delete" onClick={() => handleDelete(id)} ><DeleteIcon /></button>
 					</div>
 				</div>
 			))}
+
+			{/* <div>{blog}</div> */}
 		</div>
 	);
 }
